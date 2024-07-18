5b31cb041d8b2de6df70698f1d9060bea2d09cf8
quanlmph18475 <quanlmph18475@fpt.edu.vn>
2024-07-18 21:07:46 +0700

    update

--- src/features/Blogs/utils/truncateDescription.ts
+++ src/features/Blogs/utils/truncateDescription.ts
@@ -1,6 +1,6 @@
 export const truncateDescription = (text: string, maxLength: number) => {
-    if (text.length > maxLength) {
-      return text.substring(0, maxLength) + ' [...]'
-    }
-    return text
+  if (text.length > maxLength) {
+    return text.substring(0, maxLength) + ' [...]'
   }
+  return text
+}
